71587d1f0e56f3aefeebd121088e22f086d196dc
AlexanderTanJunAn <65282395+AlexanderTanJunAn@users.noreply.github.com>
2021-02-26 15:12:47 +0800

    Add files via upload

---
+++ Lab/myUART/myuart.d
@@ -0,0 +1,3 @@
+.\myuart.o: myUART.c
+.\myuart.o: C:\Users\alex1\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL25Z4.h
+.\myuart.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
